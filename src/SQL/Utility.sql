0857e8435a75d87bda61507062fe365eda072d2b
Uli Seelbach <ulrich.seelbach@de.ibm.com>
2025-07-22 10:04:58 +0200

    Add global variable holding utility ID.

--- src/SQL/Utility.sql
+++ src/SQL/Utility.sql
@@ -1,21 +1,22 @@
 -- Following comment lines tell Data Studio resp. SPUFI
 -- to use # as statement terminator
 --
+
+drop function db2utility(stmt clob ccsid unicode);
+drop function db2utility(utilid varchar(16), stmt clob ccsid unicode);
+drop variable utility_output;
+commit;
+
+-- Global variable to hold utility ID.
+create variable utility_id varchar(16);
+
+-- Global variable to hold output from a utility invocation.
+create variable utility_output clob(4M);
+
+commit;
+
 --<ScriptOptions statementTerminator="#"/>
 --#SET TERMINATOR #
-
-drop function db2utility(stmt clob ccsid unicode)#
-drop function db2utility(utilid varchar(16), stmt clob ccsid unicode)#
-drop variable utility_output#
-commit#
-
-/*
-Global variable to hold output from a utility invocation.
-*/
-create variable utility_output clob(4M)
-#
-
-commit#
 
 /*
 Run Db2 utility statements.
@@ -32,12 +33,13 @@ The utility output is returned in global variable UTILITY_OUTPUT.
 create function db2utility(utilid varchar(16), stmt clob ccsid unicode)
   returns integer
   modifies sql data
+  not deterministic
 begin
   declare rc     integer;
   declare offs   integer default 1;
   declare stmt_x clob default '';
   declare result clob ccsid unicode default '';
-  declare utilid varchar(12);
+  declare utilid varchar(16);
   
   -- Translate tab, linefeed and newline characters to spaces.
   -- Since TRANSLATE does not work on LOBs, we need to do this in a loop.
@@ -56,6 +58,9 @@ begin
     -- Create a random utility ID
     set utilid = 'UTIL' || digits(decimal(rand() * 100000, 5));
   end if;
+  
+  -- Remember the possibly generated utility ID in global variable
+  set utility_id = utilid;
   
   -- Call the utility
   call sysproc.dsnutilu(utilid,
@@ -85,14 +90,55 @@ end
 create function db2utility(stmt clob ccsid unicode)
   returns integer
   modifies sql data
+  not deterministic
 begin
   return db2utility(cast(null as varchar(16)), stmt);
 end
 #
 
-grant execute on function db2utility(varchar(16), clob) to public
+--<ScriptOptions statementTerminator="#"/>
+--#SET TERMINATOR #
+
+drop function terminate_utility(utilid varchar(16))
 #
+
+create function terminate_utility(utilid varchar(16))
+--  returns integer
+returns varchar(1331)
+  modifies sql data
+  not deterministic
+begin
+  declare db2_command varchar(64);
+  declare db2_command_length integer;
+  declare db2_member varchar(8) default null;
+  declare processing_type varchar(3) default '';
+  declare commands_executed, IFI_return_code, IFI_reason_code, excess_bytes,
+          group_IFI_reason_code, group_excess_bytes, return_code integer default 0;
+  declare message varchar(1331) default '';
+  
+  set db2_command = '-TERM UTILITY(' || utilid || ')';
+  set db2_command_length = length(db2_command);
+  
+  call sysproc.admin_command_db2(db2_command, 100, processing_type,
+  db2_member, commands_executed, IFI_return_code, IFI_reason_code , excess_bytes,
+  group_IFI_reason_code, group_excess_bytes, return_code, message);
+  
+  return message; --return_code * 256 + ifi_return_code;
+  
+  insert into sysibm.sysprint values(1, digits(commands_executed));
+  insert into sysibm.sysprint values(2, message);
+  set utility_output = message;
+
+--  return ifi_return_code;
+end
+#
+
+--<ScriptOptions statementTerminator=";"/>
+--#SET TERMINATOR ;
+
+grant execute on function db2utility(varchar(16), clob) to public
+;
 
 grant execute on function db2utility(clob) 
    to public
-#
+;

fc60b17ea381035430c494d7417dc0c96b273743
Uli Seelbach <ulrich.seelbach@de.ibm.com>
2025-07-22 10:09:41 +0200

    Add correlation name.

--- src/COBOL/racfprof.sql
+++ src/COBOL/racfprof.sql
@@ -6,7 +6,7 @@ create function racfprofile(dsname varchar(44), volume char(6))
   security user
   external name racfprof;
 
-select racfprofile('ADCDMST.JOB.CNTL', cast(NULL as char(6)))
+select racfprofile('ADCDMST.JOB.CNTL', cast(NULL as char(6))) as racf_profile
   from sysibm.sysdummyu;
 
 call sysproc.admin_ds_list('ADCDMST.*', 'N', 'N', 42, 'N', null, null);
